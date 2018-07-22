e6276584a73a2f514d58d585c05382ee1842fe51
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-22 17:33:15 -0300

    Rotas adicionadas e servidor funcionando

--- express-server.js
+++ express-server.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const app = express();
+const products = require('./products');
+const httpStatus = require('http-status-codes');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (req, res) => {
+  console.log(`Servidor ouvindo na porta ${port}`);
+});
+
+app.get('/products', (req, res) => {
+  res.send(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  const itens = products.filter(product => product.id == id);
+  if (!itens.length) {
+    res.statusCode = httpStatus.NO_CONTENT;
+    res.send(null);
+    return;
+  }
+  res.send(itens[0]);
+});
+
+
+
---
+++ products.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { id: 1, name: 'Coca-Cola', value: 3.50 },
+  { id: 2, name: 'Pasta de Dentes', value: 1.99 },
+  { id: 3, name: 'Sabão em pó', value: 3.50 },
+  { id: 4, name: 'Maneiga', value: 7.29 },
+  { id: 5, name: 'Kinder Ovo', value: 1000.00 },
+];
